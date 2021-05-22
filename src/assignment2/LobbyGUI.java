8c12b00cd58d4d347f941fff4f18a06bae507720
hmai2139 <hoangmai2139@gmail.com>
2021-05-23 00:44:40 +1000

    Update GUI: Separate WhiteboardGUI and LobbyGUI.

--- src/assignment2/LobbyGUI.java
+++ src/assignment2/LobbyGUI.java
@@ -14,6 +14,8 @@ public class LobbyGUI {
     private JButton usernameConfirmButton;
     private JButton createWhiteboardButton;
     private JComboBox whiteboardSelection;
+
+
     private final JFrame frame;
 
     private String user;
--- src/assignment2/WhiteboardGUI.java
+++ src/assignment2/WhiteboardGUI.java
@@ -10,17 +10,16 @@ public class WhiteboardGUI extends JFrame {
     private JPanel insertPanel;
     private JScrollPane logScrollPanel;
     private JTextPane logPanel;
-    private JPanel controlPanel;
     private JComboBox insertMenu;
     private JButton clearShapeButton;
     private JPanel stylingPanel;
     private JButton colourButton;
     private JButton switchBackgroundButton;
-    private JButton createWhiteboardButton;
-    private JComboBox whiteboardSelection;
     private JButton leaveButton;
     private JTextField textField1;
     private JLabel userSelection;
+    private JMenuBar menuBar1;
+    private JPopupMenu popupMenu1;
     private Whiteboard whiteboard;
 
     // GUI main frame.
@@ -37,96 +36,74 @@ public class WhiteboardGUI extends JFrame {
         // Get screen size.
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
+        // Initialise whiteboard.
+        whiteboard = new Whiteboard();
+        setTitle("Whiteboard");
+
+        // Customise whiteboard.
+        whiteboard.setBackground(Color.WHITE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setMinimumSize(new Dimension(screenSize.width/2, (int) (screenSize.height*0.8)));
+        this.setLocation(5, screenSize.height/20);
+        this.add(whiteboard);
+        colourButton.setBackground(whiteboard.colour());
+        this.setVisible(true);
+
         // User interface's initialisation and customisations.
         frame = new JFrame("User interface");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setMinimumSize(new Dimension(screenSize.width / 2, screenSize.width / 2));
-        frame.setLocationRelativeTo(null);
+        frame.setMinimumSize(new Dimension(screenSize.width / 2,(int) (screenSize.height*0.8)));
+        frame.setLocation(this.getX() + this.getWidth(), screenSize.height/20);
 
-        // Allows user to create a new whiteboard and become its manage by default.
-        createWhiteboardButton.addActionListener(e -> {
-            // Initialise whiteboard.
-            whiteboard = new Whiteboard();
-            setTitle("Whiteboard");
-
-            // Customise whiteboard.
-            whiteboard.setBackground(Color.WHITE);
-            //setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            this.setMinimumSize(new Dimension(screenSize.width / 2, screenSize.width / 2));
-            this.setLocationRelativeTo(null);
-            this.add(whiteboard);
-            this.setVisible(true);
-            colourButton.setBackground(whiteboard.colour());
-
-            // Disable Create button.
-            createWhiteboardButton.setEnabled(false);
-
-            // Enable Leave button.
-            leaveButton.setEnabled(true);
-
-            this.addListeners();
-
-            // Revalidate the frame.
-            frame.revalidate();
-        });
-    }
-
-    // Add necessary listeners.
-    public void addListeners() {
-        // Enable necessary listeners for whiteboards.
-        if (whiteboard != null) {
-            // Behaviours when user clicks on whiteboard.
-            whiteboard.addMouseListener(new MouseAdapter() {
-                public void mousePressed(MouseEvent e) {
-                    if (whiteboard.mode() != Mode.TEXT) {
-                        return;
-                    }
-                    // Focus on the clicked text field.
-                    if (e.getClickCount() == 1) {
-                        whiteboard.requestFocusInWindow();
-                    }
-                    // Create a new text field when user double-clicks on whiteboard.
-                    if (e.getClickCount() == 2) {
-                        TextField textField = new TextField(whiteboard);
-                        textField.setLocation(e.getPoint());
-                        add(textField);
-                        textField.requestFocusInWindow();
-                    }
-                }
-            });
-
-            // Select an insert option.
-            insertMenu.addActionListener(e -> {
-                JComboBox insertMenu = (JComboBox) e.getSource();
-                Mode mode = Mode.valueOf((String) insertMenu.getSelectedItem());
-                whiteboard.setMode(mode);
-            });
-
-            // Select a colour and update current colour indicator.
-            colourButton.addActionListener(e -> {
-                Color colour = whiteboard.colour();
-                colour = JColorChooser.showDialog(frame, "Select a colour", colour);
-                if (colour != null) {
-                    whiteboard.setColour(colour);
-                    colourButton.setBackground(colour);
-                }
-            });
-
-            // Clear all drawn shapes from whiteboard.
-            clearShapeButton.addActionListener(e -> whiteboard.clear());
-
-            // Switch background.
-            switchBackgroundButton.addActionListener(e -> {
-                whiteboard.switchGrid();
-            });
-        }
+        // Leave the whiteboard.
         leaveButton.addActionListener(e -> {
-            remove(whiteboard);
-            createWhiteboardButton.setEnabled(true);
-            leaveButton.setEnabled(false);
-            repaint();
-            dispose();
+            System.exit(0);
+        });
+
+        // Insert text at the location of user's cursor.
+        whiteboard.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                if (whiteboard.mode() != Mode.TEXT) {
+                    return;
+                }
+                // Focus on the clicked text field.
+                if (e.getClickCount() == 1) {
+                    whiteboard.requestFocusInWindow();
+                }
+                // Create a new text field when user double-clicks on whiteboard.
+                if (e.getClickCount() == 2) {
+                    TextField textField = new TextField(whiteboard);
+                    textField.setLocation(e.getPoint());
+                    add(textField);
+                    textField.requestFocusInWindow();
+                }
+            }
+        });
+
+        // Select an insert option.
+        insertMenu.addActionListener(e -> {
+            JComboBox insertMenu = (JComboBox) e.getSource();
+            Mode mode = Mode.valueOf((String) insertMenu.getSelectedItem());
+            whiteboard.setMode(mode);
+        });
+
+        // Select a colour and update current colour indicator.
+        colourButton.addActionListener(e -> {
+            Color colour = whiteboard.colour();
+            colour = JColorChooser.showDialog(frame, "Select a colour", colour);
+            if (colour != null) {
+                whiteboard.setColour(colour);
+                colourButton.setBackground(colour);
+            }
+        });
+
+        // Clear all drawn shapes from whiteboard.
+        clearShapeButton.addActionListener(e -> whiteboard.clear());
+
+        // Switch background.
+        switchBackgroundButton.addActionListener(e -> {
+            whiteboard.switchGrid();
         });
     }
 
